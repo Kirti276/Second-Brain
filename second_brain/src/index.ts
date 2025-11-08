931a140d27a5d2377634bbfdd56897af3089e9e6
Kirti Bansal <bansalkirti2810@gmail.com>
2025-11-08 19:50:34 +0530

    updated index.ts

--- second_brain/src/index.ts
+++ second_brain/src/index.ts
@@ -201,4 +201,7 @@ app.get("/api/v1/brain/:shareLink", async (req,res)=>{
 
 
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
