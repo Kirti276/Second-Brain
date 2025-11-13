ba693a220e9830da309ef17c98f868037e3f26a6
Hemlata55 <yadavhemlata559@gmail.com>
2025-11-13 15:54:16 +0530

    Integrate AI chatbot frontend component

---
+++ brainly_frontend/src/icons/BrainIcon.tsx
@@ -0,0 +1,20 @@
+
+
+export function BrainIcon({ size = 24, color = "currentColor" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M15 4a3 3 0 0 0-3-3v0a3 3 0 0 0-3 3v0a3 3 0 0 0-3 3v0a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3v0a3 3 0 0 0-3-3v0z" />
+      <path d="M9 21a3 3 0 0 0 3 3v0a3 3 0 0 0 3-3v0a3 3 0 0 0 3-3v0a3 3 0 0 0-3-3H9a3 3 0 0 0-3 3v0a3 3 0 0 0 3 3v0z" />
+    </svg>
+  );
+}
